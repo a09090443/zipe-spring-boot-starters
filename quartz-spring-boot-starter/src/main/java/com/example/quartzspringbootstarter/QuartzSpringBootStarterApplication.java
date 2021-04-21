2dc6e2e27b6294cacf1f3ad21be0247e8ee970a2
zipe <zipe.daden@gmail.com>
2021-04-21 22:30:25 +0800

    1. Initial project.

---
+++ quartz-spring-boot-starter/src/main/java/com/example/quartzspringbootstarter/QuartzSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.quartzspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzSpringBootStarterApplication.class, args);
+    }
+
+}
