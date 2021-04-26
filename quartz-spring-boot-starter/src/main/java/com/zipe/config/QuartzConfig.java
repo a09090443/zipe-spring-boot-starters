5bb3ec5fe3624f0bf46d03d8bebc46adc20e6e8e
zipe <zipe.daden@gmail.com>
2021-04-26 17:42:30 +0800

    1. Use default setting to create example job.

--- quartz-spring-boot-starter/src/main/java/com/zipe/config/QuartzConfig.java
+++ quartz-spring-boot-starter/src/main/java/com/zipe/config/QuartzConfig.java
@@ -1,34 +1,87 @@
 package com.zipe.config;
 
 import com.zipe.job.TestJob;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.SimpleScheduleBuilder;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import java.util.Objects;
 
 /**
- * @author zipe
- */
+ * @author : Gary Tsai
+ * @created : @Date 2020/11/20 上午 10:16
+ **/
 @Configuration
 public class QuartzConfig {
 
-    @Bean
-    public JobDetail testQuartz1() {
-        return JobBuilder.newJob(TestJob.class).withIdentity("testTask1").storeDurably().build();
+    private final JobFactory jobFactory;
+
+    private final Environment env;
+
+    @Autowired
+    public QuartzConfig(JobFactory jobFactory, Environment env) {
+        this.jobFactory = jobFactory;
+        this.env = env;
     }
 
     @Bean
-    public Trigger testQuartzTrigger1() {
-        //5秒执行一次
-        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
-                .withIntervalInSeconds(5)
-                .repeatForever();
-        return TriggerBuilder.newTrigger().forJob(testQuartz1())
-                .withIdentity("testTask1")
-                .withSchedule(scheduleBuilder)
-                .build();
+    public JobDetailFactoryBean testJob() {
+        JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
+        jobDetailFactoryBean.setDurability(true);
+        jobDetailFactoryBean.setJobClass(TestJob.class);
+        return jobDetailFactoryBean;
+    }
+
+    //    @Bean
+//    public JobDetailFactoryBean invoiceUnusedUploadJob() {
+//        JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
+//        jobDetailFactoryBean.setDurability(true);
+//        jobDetailFactoryBean.setJobClass(InvoiceUnusedUploadJob.class);
+//        return jobDetailFactoryBean;
+//    }
+//
+//    @Bean
+//    public JobDetailFactoryBean turnkeyDownloadResultJob() {
+//        JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
+//        jobDetailFactoryBean.setDurability(true);
+//        jobDetailFactoryBean.setJobClass(TurnkeyDownloadResultJob.class);
+//        return jobDetailFactoryBean;
+//    }
+//
+    @Bean
+    public CronTriggerFactoryBean invoiceUploadJobCornTrigger() {
+        CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
+        cronTriggerFactoryBean.setJobDetail(Objects.requireNonNull(testJob().getObject()));
+        cronTriggerFactoryBean.setCronExpression("*/5 * * * * ?");
+        return cronTriggerFactoryBean;
+    }
+
+    //
+//    @Bean
+//    public CronTriggerFactoryBean invoiceUnusedUploadJobCornTrigger() {
+//        CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
+//        cronTriggerFactoryBean.setJobDetail(Objects.requireNonNull(invoiceUnusedUploadJob().getObject()));
+//        cronTriggerFactoryBean.setCronExpression("0 0 23 9 1,3,5,7,9,11 ?");
+//        return cronTriggerFactoryBean;
+//    }
+//
+//    @Bean
+//    public CronTriggerFactoryBean turnkeyDownloadResultJobCornTrigger() {
+//        CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
+//        cronTriggerFactoryBean.setJobDetail(Objects.requireNonNull(turnkeyDownloadResultJob().getObject()));
+//        cronTriggerFactoryBean.setCronExpression("0 15,45 * * * ?");
+//        return cronTriggerFactoryBean;
+//    }
+//
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        schedulerFactoryBean.setJobFactory(jobFactory);
+        schedulerFactoryBean.setTriggers(invoiceUploadJobCornTrigger().getObject());
+        return schedulerFactoryBean;
     }
 }
--- quartz-spring-boot-starter/src/main/java/com/zipe/job/TestJob.java
+++ quartz-spring-boot-starter/src/main/java/com/zipe/job/TestJob.java
@@ -4,10 +4,13 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+/**
+ * @author : Gary Tsai
+ * @created : @Date 2021/4/26 下午 05:38
+ **/
 public class TestJob extends QuartzJobBean {
-
     @Override
-    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        System.out.println("Job exec!");
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("test");
     }
 }
