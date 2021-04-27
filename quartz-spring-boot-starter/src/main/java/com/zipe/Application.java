73472423e5dcadbb090c45cc2f26d0d0ceca0dd6
zipe <zipe.daden@gmail.com>
2021-04-27 16:44:02 +0800

    1. Rebuild fo database support.

--- quartz-spring-boot-starter/src/main/java/com/zipe/Application.java
+++ quartz-spring-boot-starter/src/main/java/com/zipe/Application.java
@@ -2,8 +2,10 @@ package com.zipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:spring-quartz.properties", "classpath:quartz-h2.properties"})
 public class Application {
 
     public static void main(String[] args) {
