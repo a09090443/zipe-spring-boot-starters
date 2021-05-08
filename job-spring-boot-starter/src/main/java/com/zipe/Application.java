0f8415768c43046d39a209ded1207bfbc4549fa8
zipe <zipe.daden@gmail.com>
2021-05-08 21:53:02 +0800

    1. Adjust code.

--- job-spring-boot-starter/src/main/java/com/zipe/Application.java
+++ job-spring-boot-starter/src/main/java/com/zipe/Application.java
@@ -2,14 +2,24 @@ package com.zipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .initializers(new ApplicationContextInitializer<GenericApplicationContext>() {
+                    @Override
+                    public void initialize(GenericApplicationContext applicationContext) {
+                        applicationContext.setAllowBeanDefinitionOverriding(false);
+                    }
+                })
+                .run(args);
     }
 
 }
