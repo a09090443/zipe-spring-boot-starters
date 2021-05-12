7047ea8519565f80feae05b93553b6c2b23e7308
zipe <zipe.daden@gmail.com>
2021-05-12 10:47:35 +0800

    1. Refactoring code. 3. Add comments.

--- logon-spring-boot-starter/src/main/java/com/zipe/Application.java
+++ logon-spring-boot-starter/src/main/java/com/zipe/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
