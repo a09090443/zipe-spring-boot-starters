8af7e471e7cec159fc66279cd6d36ce217f86c27
zipe <zipe.daden@gmail.com>
2021-05-12 20:47:56 +0800

    1. Fix cloud not reach web service wsdl.

--- web-service-spring-boot-starter/src/main/java/com/zipe/autoconfiguration/CxfConfigAutoConfiguration.java
+++ web-service-spring-boot-starter/src/main/java/com/zipe/autoconfiguration/CxfConfigAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.zipe.autoconfiguration;
 
 import com.zipe.config.WebServicePropertyConfig;
 import org.apache.cxf.transport.servlet.CXFServlet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
@@ -18,6 +19,7 @@ public class CxfConfigAutoConfiguration {
 
     private final WebServicePropertyConfig webServicePropertyConfig;
 
+    @Autowired
     public CxfConfigAutoConfiguration(WebServicePropertyConfig webServicePropertyConfig) {
         this.webServicePropertyConfig = webServicePropertyConfig;
     }
