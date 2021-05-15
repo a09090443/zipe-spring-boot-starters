404a48150ded2ffc3cd7b7e6f290141b90e73047
zipe <zipe.daden@gmail.com>
2021-05-15 14:34:04 +0800

    1. Fix web-spring-boot-starter cloud not start.

--- web-spring-boot-starter/src/main/java/com/zipe/autoconfiguration/TomcatAutoConfiguration.java
+++ web-spring-boot-starter/src/main/java/com/zipe/autoconfiguration/TomcatAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.zipe.autoconfiguration;
 import org.apache.catalina.Context;
 import org.apache.tomcat.util.scan.StandardJarScanner;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
  * @author Gary Tsai
  */
 @Configuration
+@ConditionalOnClass({WebAutoConfiguration.class})
 @AutoConfigureBefore(ServletWebServerFactoryAutoConfiguration.class)
 public class TomcatAutoConfiguration {
     @Bean
