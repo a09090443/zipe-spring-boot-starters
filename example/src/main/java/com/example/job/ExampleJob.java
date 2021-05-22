ad471abafb7cb3cd008cfa99c6ff0c3e9c5a9925
zipe <zipe.daden@gmail.com>
2021-05-22 22:19:58 +0800

    Fix quartz class name.

--- example/src/main/java/com/example/job/ExampleJob.java
+++ example/src/main/java/com/example/job/ExampleJob.java
@@ -1,6 +1,6 @@
 package com.example.job;
 
-import com.zipe.quartz.job.QuartJobFactory;
+import com.zipe.quartz.job.QuartzJobFactory;
 import com.zipe.util.time.DateTimeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobExecutionContext;
@@ -9,7 +9,7 @@ import org.quartz.JobExecutionContext;
  * @author Gary.Tsai
  */
 @Slf4j
-public class ExampleJob extends QuartJobFactory {
+public class ExampleJob extends QuartzJobFactory {
 
     public ExampleJob() {
     }
