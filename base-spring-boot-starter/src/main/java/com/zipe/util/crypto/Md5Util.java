7d267dc5ff28e17d2d432341e9cde1a7b872ef09
Gary Tsai <zipe.daden@gmail.com>
2021-08-11 16:20:12 +0800

    1. Add MD5 util.

---
+++ base-spring-boot-starter/src/main/java/com/zipe/util/crypto/Md5Util.java
@@ -0,0 +1,66 @@
+package com.zipe.util.crypto;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Md5Util {
+    /**
+     * @param str
+     * @Description: 32位小寫MD5
+     */
+    public static String parseStrToMd5L32(String str) {
+        String reStr = null;
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] bytes = md5.digest(str.getBytes());
+            StringBuffer stringBuffer = new StringBuffer();
+            for (byte b : bytes) {
+                int bt = b & 0xff;
+                if (bt < 16) {
+                    stringBuffer.append(0);
+                }
+                stringBuffer.append(Integer.toHexString(bt));
+            }
+            reStr = stringBuffer.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return reStr;
+    }
+
+    /**
+     * @param str
+     * @Description: 32位大寫MD5
+     */
+    public static String parseStrToMd5U32(String str) {
+        String reStr = parseStrToMd5L32(str);
+        if (reStr != null) {
+            reStr = reStr.toUpperCase();
+        }
+        return reStr;
+    }
+
+    /**
+     * @param str
+     * @Description: 16位小寫MD5
+     */
+    public static String parseStrToMd5U16(String str) {
+        String reStr = parseStrToMd5L32(str);
+        if (reStr != null) {
+            reStr = reStr.toUpperCase().substring(8, 24);
+        }
+        return reStr;
+    }
+
+    /**
+     * @param str
+     * @Description: 16位大寫MD5
+     */
+    public static String parseStrToMd5L16(String str) {
+        String reStr = parseStrToMd5L32(str);
+        if (reStr != null) {
+            reStr = reStr.substring(8, 24);
+        }
+        return reStr;
+    }
+}
