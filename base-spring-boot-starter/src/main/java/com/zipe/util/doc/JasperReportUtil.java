357f42f6e99682633d844ddb586f1b3c7bc1eb0e
Zipe <zipe.daden@gmail.com>
2023-05-13 14:56:22 +0800

    1. Fix Jasperreport cloud not produce file bugs.

--- base-spring-boot-starter/src/main/java/com/zipe/util/doc/JasperReportUtil.java
+++ base-spring-boot-starter/src/main/java/com/zipe/util/doc/JasperReportUtil.java
@@ -1,5 +1,8 @@
 package com.zipe.util.doc;
 
+import java.io.FileNotFoundException;
+import java.util.Objects;
+import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
@@ -14,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Map;
+import org.springframework.util.ResourceUtils;
 
 /**
  * @author : Gary Tsai
@@ -21,19 +25,28 @@ import java.util.Map;
  **/
 public class JasperReportUtil {
 
-    public static byte[] exportPdfFile(String jrxmlFile, Map<String, Object> params) throws JRException {
+  public static byte[] exportPdfFile(String jrxmlFile, Map<String, Object> params,
+      JRDataSource dataSource) throws JRException, FileNotFoundException {
 
-        ClassLoader classLoader = JasperReportUtil.class.getClassLoader();
-        InputStream inputStream = classLoader.getResourceAsStream(jrxmlFile);
+    File template = ResourceUtils.getFile(jrxmlFile);
 
-        JasperReport jasperDesign = JasperCompileManager.compileReport(inputStream);
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperDesign, params, new JREmptyDataSource());
+    JasperReport jasperDesign = JasperCompileManager.compileReport(template.getAbsolutePath());
+    JasperPrint jasperPrint = JasperFillManager.fillReport(jasperDesign, params,
+        Objects.nonNull(dataSource) ? dataSource : new JREmptyDataSource());
 
-        return JasperExportManager.exportReportToPdf(jasperPrint);
-    }
+    return JasperExportManager.exportReportToPdf(jasperPrint);
+  }
 
-    public static void exportPdfFile(String jrxmlFile, File exportFile, Map<String, Object> params) throws IOException, JRException {
-        OutputStream outputSteam = new FileOutputStream(exportFile);
-        outputSteam.write(exportPdfFile(jrxmlFile, params));
-    }
+  public static void exportPdfFile(String jrxmlFile, File exportFile, Map<String, Object> params)
+      throws IOException, JRException {
+    OutputStream outputSteam = new FileOutputStream(exportFile);
+    outputSteam.write(exportPdfFile(jrxmlFile, params, null));
+  }
+
+  public static void exportPdfFile(String jrxmlFile, File exportFile, Map<String, Object> params,
+      JRDataSource dataSource) throws IOException, JRException {
+    OutputStream outputSteam = new FileOutputStream(exportFile);
+    outputSteam.write(exportPdfFile(jrxmlFile, params, dataSource));
+  }
+
 }
