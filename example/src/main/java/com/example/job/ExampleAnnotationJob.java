f844a4b6bcdff3087ab28cda01656789bdac8d06
Gary <zipe.daden@gmail.com>
2024-01-13 22:40:08 +0800

    Modify quartz setting.

--- example/src/main/java/com/example/job/ExampleAnnotationJob.java
+++ example/src/main/java/com/example/job/ExampleAnnotationJob.java
@@ -1,16 +1,16 @@
 package com.example.job;
 
+import com.zipe.util.time.DateTimeUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-/**
- * @author Gary.Tsai
- */
+@Slf4j
 @Component
 public class ExampleAnnotationJob {
 
     @Scheduled(cron = "0/20 * * * * ?")
-    public void example(){
-        System.out.println("example");
+    public void example() {
+        log.info("{}執行, 當前的時間:{}", this.getClass(), DateTimeUtils.getDateNow());
     }
 }
--- example/src/main/java/com/example/job/ExampleXmlJob.java
+++ example/src/main/java/com/example/job/ExampleXmlJob.java
@@ -5,17 +5,14 @@ import com.zipe.util.time.DateTimeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobExecutionContext;
 
-/**
- * @author Gary.Tsai
- */
 @Slf4j
-public class ExampleJob extends QuartzJobFactory {
+public class ExampleXmlJob extends QuartzJobFactory {
 
-    public ExampleJob() {
+    public ExampleXmlJob() {
     }
 
     @Override
-    protected void executeJob(JobExecutionContext jobExecutionContext) throws Exception {
+    protected void executeJob(JobExecutionContext jobExecutionContext) {
         log.info("{}執行, 當前的時間:{}", this.getClass(), DateTimeUtils.getDateNow());
     }
 }
