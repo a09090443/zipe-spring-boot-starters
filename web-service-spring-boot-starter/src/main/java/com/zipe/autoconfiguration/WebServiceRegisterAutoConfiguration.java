b42741678348ea4c1241efbb46a2a2ff1db6ca10
Gary <zipe.daden@gmail.com>
2024-08-28 21:24:53 +0800

    忽略 xmlns="" 格式

--- web-service-spring-boot-starter/src/main/java/com/zipe/autoconfiguration/WebServiceRegisterAutoConfiguration.java
+++ web-service-spring-boot-starter/src/main/java/com/zipe/autoconfiguration/WebServiceRegisterAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.zipe.autoconfiguration;
 import com.zipe.config.WebServicePropertyConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.cxf.Bus;
+import org.apache.cxf.jaxb.JAXBDataBinding;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
@@ -12,6 +13,9 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Gary Tsai
@@ -40,12 +44,32 @@ public class WebServiceRegisterAutoConfiguration implements InitializingBean, Ap
         webServicePropertyConfig.getMap().forEach((k, v) -> {
             EndpointImpl endpoint;
             try {
-                endpoint = new EndpointImpl(bus, applicationContext.getBean(v.getBeanName()));
+                Object implementor = applicationContext.getBean(v.getBeanName());
+                endpoint = new EndpointImpl(bus, implementor);
+
+                // Configure to ignore empty namespaces
+                Map<String, Object> properties = new HashMap<>();
+                properties.put("set-jaxb-validation-event-handler", "false");
+
+                endpoint.setProperties(properties);
+
+                // Set custom JAXBDataBinding
+                JAXBDataBinding jaxbDataBinding = new JAXBDataBinding();
+                jaxbDataBinding.setUnwrapJAXBElement(true);
+                jaxbDataBinding.setMtomEnabled(true);
+
+                // Configure namespace mapping to ignore empty namespaces
+                Map<String, String> nsMap = new HashMap<>();
+                nsMap.put("", "http://www.w3.org/2001/XMLSchema");
+                jaxbDataBinding.setNamespaceMap(nsMap);
+
+                endpoint.setDataBinding(jaxbDataBinding);
+
                 endpoint.publish(v.getUriMapping());
                 log.info("Web Service 註冊服務:{}, 對應 URI:{}", v.getBeanName(), v.getUriMapping());
             } catch (Exception e) {
                 log.error("Web Service 註冊服務:{}, 失敗", v.getBeanName());
-                throw e;
+                throw new RuntimeException(e);
             }
 
         });
